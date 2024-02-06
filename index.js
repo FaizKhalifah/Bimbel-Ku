fcfaf9616d6284f2976fadc8856a05b682d15e8d
Muhammad Alfaiz Khalifah Alamsyah <faiz_doc@outlook.com>
2024-02-06 22:24:41 +0700

    connect ke database

--- index.js
+++ index.js
@@ -6,3 +6,30 @@ const input = readlinePromises.createInterface({
     input:process.stdin,
     output:process.stdout
 })
+
+async function main(){
+
+}
+
+const client = new MongoClient('mongodb://localhost:27017/BimbelKu');
+
+async function fetchMurid(){
+    await client.connect();
+    const db = client.db();
+    const murid = db.collection('Murid');
+    return murid;
+}
+
+async function fetchGuru(){
+    await client.connect();
+    const db = client.db();
+    const guru = db.collection('Guru');
+    return guru;
+}
+
+async function fetchPelajaran(){
+    await client.connect();
+    const db = client.db();
+    const pelajaran = db.collection('Pelajaran');
+    return pelajaran;
+}
