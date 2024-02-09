00f370d79f7a2ebd8a73771f400abe0505c24c45
Muhammad Alfaiz Khalifah Alamsyah <faiz_doc@outlook.com>
2024-02-09 22:22:45 +0700

    memperbaiki fungsi pilihpelajaran serta menambahkan fungsi checkpelajaran dan findmurid

--- Controllers/controllerMurid.js
+++ Controllers/controllerMurid.js
@@ -39,11 +39,50 @@ async function pilihPelajaran(namaMurid,idMurid,namaPelajaran,idPelajaran){
         console.log("Mata pelajaran tidak ditemukan");
         return;
     }else{
-        await Murid.updateOne(murid,{$push:{mataPelajaran:namaPelajaran}});
+        if(await checkPelajaran(namaMurid,idMurid,namaPelajaran,idPelajaran)==false){
+           console.log("Pelajaran sudah pernah ditambah");
+           return; 
+        }
+        await Murid.updateOne(murid,{$push:{mataPelajaran:pelajaran}});
         await Pelajaran.updateOne(pelajaran,{$push:{daftarMurid:namaMurid}});
         console.log("Kamu berhasil bergabung ke pelajaran");
         return;
     }
+
 }
 
-await pilihPelajaran("Faiz","1213131","pemweb","pw123123");
+async function checkPelajaran(namaMurid,idMurid,namaPelajaran,idPelajaran){
+    const pelajaran = {
+        nama:namaPelajaran,
+        id:idPelajaran
+    }
+    const murid = {
+        nama:namaMurid,
+        id:idMurid
+    }
+    const arrayPelajaran = await Murid.findOne(murid,{mataPelajaran:1});
+    for(let i in arrayPelajaran.mataPelajaran){
+        if(arrayPelajaran.mataPelajaran[i].nama==namaPelajaran && arrayPelajaran.mataPelajaran[i].id==idPelajaran){
+            return false;
+        }
+    }
+    return true;
+   
+}
+
+async function findMurid(nama,id){
+    const murid = {
+        nama:nama,
+        id:id
+    }
+    const status = await Murid.findOne(murid);
+    if(status==null){
+        return false;
+    }else{
+        return true;
+    }
+}
+
+const status = await pilihPelajaran("Faiz","1213131","pemweb","pw123123");
+console.log(status);
+
